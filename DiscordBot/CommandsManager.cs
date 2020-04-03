bbee851abc69de136bd7cd3bf71c1b17da7d0b3b
CheersWorld <35735834+CheersWorld@users.noreply.github.com>
2020-04-03 13:12:45 +0200

    Add files via upload

---
+++ DiscordBot/CommandsManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class CommandsManager
+    {
+        ItemsSingleton UsedItemsSingleton = ItemsSingleton.Instance;
+
+        [Command("SetMyArticle")]
+        public async Task AddArticle(CommandContext ctx, string Message)
+        {
+            UsedItemsSingleton.SetArticle(ctx.User.ToString(), Message);
+            await ctx.RespondAsync("Dein Artikel ist: " + Message);
+        }
+
+        [Command("GetMyArticle")]
+        public async Task GetArticle(CommandContext ctx)
+        {
+            await ctx.RespondAsync("Dein Artikel ist: " + UsedItemsSingleton.GetArticle(ctx.User.ToString()));
+        }
+
+        [Command("GetRandomArticle")]
+        public async Task RespondArticle(CommandContext ctx)
+        {
+            String[] _usedArray = new string[UsedItemsSingleton.GetCount()];
+            int i = 0;
+            foreach (string Article in UsedItemsSingleton.GetDictionary().Values)
+            {
+                _usedArray[i] = Article;
+                i++;
+            }
+            Console.WriteLine("Returned random Article");
+            await ctx.RespondAsync("Wort ist: " + _usedArray[new Random().Next(0, UsedItemsSingleton.GetDictionary().Count())]);
+        }
+        [Command("helpme")]
+        public async Task Help(CommandContext ctx)
+        {
+            await ctx.RespondAsync("Die gute alte Hilfestellung. Um einen Artikel hinzuzufügen, schick eine Nachricht mit !SetMyArticle gefolgt von deinem Artikel. Um deinen eingegeben Artikel zu sehen, schick eine Nachricht mit !GetMyArticle. Beide Kommandos funktionieren im Privat-Chat. Aus den eingereichten Artikeln wird mit !GetRandomArticle ein Artikel ausgewählt.");
+        }
+    }
+}
---
+++ DiscordBot/ItemsSingleton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public sealed class ItemsSingleton
+    {
+        private static readonly Lazy<ItemsSingleton>
+            lazy =
+            new Lazy<ItemsSingleton>
+                (() => new ItemsSingleton());
+
+        public static ItemsSingleton Instance { get { return lazy.Value; } }
+        int i = 0;
+        Dictionary<string, string> UsedThings = new Dictionary<string, string>();
+        private ItemsSingleton()
+        {
+
+        }
+        public void SetArticle(string _key, string _value)
+        {
+            try
+            {
+                UsedThings.Add(_key.Split(' ')[2], _value);
+                Console.Write("New Article added. " + _key.Split(' ')[2] + " " + _value + "\r\n");
+            } catch
+            {
+                Console.Write("Article updated. " + _key.Split(' ')[2] + " " + _value + "\r\n");
+                UsedThings[_key.Split(' ')[2]] = _value;
+            }
+        }
+        public string GetArticle(string _key)
+        {
+            Console.Write("Article returned for. " + _key.Split(' ')[2]);
+            return UsedThings[_key.Split(' ')[2]];
+        }
+        public int GetCount()
+        {
+            return UsedThings.Count();
+        }
+        public Dictionary<string, string> GetDictionary()
+        {
+            return UsedThings;
+        }
+    }
+}
